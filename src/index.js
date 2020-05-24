3fe9be5f3b788618575db5874d258275713acaf3
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-05-24 11:27:21 -0300

    Criando rotas do app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
 import './config/ReactotronConfig';
 
 export default function App() {
   return (
-    <View>
-      <Text>Welcome to React Native</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
